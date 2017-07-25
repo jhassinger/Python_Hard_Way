2031548d24a2c603a1666cc8676eee6ab2248060
Julian Hassinger <j.e.hassinger@gmail.com>
2017-07-25 14:59:02 -0700

    Exercise 1
    Completed exercise 1.

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# This is how to do a comment
+print "Hello World!"
+print "Hello again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printin.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
