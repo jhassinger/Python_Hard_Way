710df60526aa80fffd73dfaaae692c116953af58
Julian Hassinger <j.e.hassinger@gmail.com>
2017-07-26 16:39:18 -0700

    Exercise 7
    More printing

---
+++ ex7.py
@@ -0,0 +1,22 @@
+print "Mary had a little lamb."	# Print a string
+print "Its fleece was white as %s." % 'snow' # Print a string with another string formatted in
+print "And everywhere that Mary went." # Print a string
+print "." * 10 # what'd that do?, Makes ten periods
+
+# Assigning a bunch of string variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# try removing comma at end, keeps everything on one line
+print end1 + end2 + end3 + end4 + end5 + end6, # Print end1-end6 combined
+print end7 + end8 + end9 + end10 + end11 + end12 # Print end7-end12 combined
